3fac5c4a8b20da2b58cafa7c01d7933b758646c1
Minli Guo <142051206+Gminli@users.noreply.github.com>
2024-07-16 13:36:33 -0400

    Update Assignment 2.R

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,3 +1,7 @@
+# Data Analysis using Bass Model to predict new product diffusion patterns
+# This dataset contains the daily count of number of tweets containing words "dove", "real", "beauty", and "sketches", during the first 21 days after the release of the ad “dove real beauty sketches”. 
+
+
 library(rtweet)
 library(data.table)
 install.packages("ggplot2")
@@ -6,17 +10,17 @@ install.packages("devtools")
 dove_daily=read.csv("dove_daily.csv")
 View(dove_daily)
 library(diffusion)
-# Q1
+# Q1 Generate the summary statistics for the daily count of number of tweets. Discuss your outputs.
 #summary statistics for the daily count of number of tweets
 summary(dove_daily)
 
-#Q2
+#Q2 Plot the daily count of number of tweets over time. Discuss your plot. 
 # Trend Analysis: find out the number of tweets over time
 # Times Series Analysis
 par(mar=c(3,3,3,3))
 plot(dove_daily$Dove_real_beauty_sketches)
 
-#Q3
+#Q3 Fit a bass model based on the first 8 days of data, plot the output, and describe the plot. 
 #select first 8 days of data
 temp=dove_daily[1:8, ]
 View(temp)
