a523cc4b8583cdb4f6795ef770431447aa6c66dc
Minli Guo <142051206+Gminli@users.noreply.github.com>
2024-07-16 13:45:45 -0400

    Add files via upload

---
+++ SCRIPT.R
@@ -0,0 +1,35 @@
+# Script for final exam part 2  
+# Fill in with your CODES AND ANSWERS 
+
+
+########################### Preliminaries ############################# 
+# Your codes in lines below
+
+
+
+
+########################### Task i  ###################################
+# Your written comments here (if any)
+# 
+# 
+# Your codes in lines below
+
+
+
+
+########################### Task ii  ###################################
+# Your written comments here (if any)
+# 
+# 
+# Your codes in lines below
+
+
+
+
+########################### Task iii  ###################################
+# Your written comments here (if any)
+# 
+# 
+# Your codes in lines below
+
+
